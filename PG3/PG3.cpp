537dfddc46300602d34cf1895449059812af3223
Taka <k022g0068@g.neec.ac.jp>
2024-02-08 22:58:17 +0900

    完了

--- PG3/PG3.cpp
+++ PG3/PG3.cpp
@@ -1,25 +1,34 @@
+#include<stdio.h>
 #include <iostream>
-#include<thread>
-#include<queue>
-#include<condition_variable>
-#include<mutex>
+#include<chrono>
 
-
-void Thread(int num) {
-	printf("thread %d \n", num);
-}
 
 int main()
 {
 
-	std::mutex mutex;
-
-	std::condition_variable condition;
-
-	std::queue<int>q;
+	std::string data(100000, 'a');
+	std::cout << "コピーと移動の比較  : "<< std::endl;
 
 
+	auto st = std::chrono::high_resolution_clock::now();
 
+	std::string copy = data;
+
+	auto end = std::chrono::high_resolution_clock::now();
+	auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end - st);
+	long second = duration.count();
+
+	std::cout << "コピー : " << second << std::endl;
+
+	st = std::chrono::high_resolution_clock::now();
+
+	std::string move = std::move(data);
+
+	end = std::chrono::high_resolution_clock::now();
+	duration = std::chrono::duration_cast<std::chrono::microseconds>(end - st);
+	second = duration.count();
+	
+	std::cout << "移動 : " << second <<  std::endl;
 
 	return 0;
 }
