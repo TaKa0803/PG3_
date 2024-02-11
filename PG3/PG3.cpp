9dbdb239fa2ff1f284febf7c2026743d82436062
Taka <k022g0068@g.neec.ac.jp>
2024-02-11 23:39:46 +0900

    kan

--- PG3/PG3.cpp
+++ PG3/PG3.cpp
@@ -2,33 +2,49 @@
 #include <iostream>
 #include<chrono>
 
+template<typename T,typename T2>
+
+class Num {
+
+public:
+
+	T num1_;
+	T2 num2_;
+
+	Num(T num1, T2 num2) :num1_(num1), num2_(num2) {}
+
+	T2 Min() {
+		if (num1_ >= (T)num2_) {
+			return (T2)num2_;
+		}
+		else {
+			return (T)num1_;
+		}
+	}
+
+
+};
+
 
 int main()
 {
 
-	std::string data(100000, 'a');
-	std::cout << "コピーと移動の比較  : "<< std::endl;
+	Num<int,int> b1(10, 20);
+	Num<float,float>b2(1.2f, 1.4f);
+	Num<double,double>b3(1.2, 1.4);
 
-
-	auto st = std::chrono::high_resolution_clock::now();
-
-	std::string copy = data;
-
-	auto end = std::chrono::high_resolution_clock::now();
-	auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end - st);
-	long second = duration.count();
-
-	std::cout << "コピー : " << second << std::endl;
-
-	st = std::chrono::high_resolution_clock::now();
-
-	std::string move = std::move(data);
-
-	end = std::chrono::high_resolution_clock::now();
-	duration = std::chrono::duration_cast<std::chrono::microseconds>(end - st);
-	second = duration.count();
+	Num<int, float>b4(3, 1.4f);
+	Num<int, double>b5(5, 1.4);
+	Num<double, float>b6(1.2, 1.4f);
 	
-	std::cout << "移動 : " << second <<  std::endl;
 
-	return 0;
+
+	printf("小さい値 : %d\n", b1.Min());
+	printf("小さい値 : %f\n", b2.Min());
+	printf("小さい値 : %f\n", b3.Min());
+
+	printf("小さい値 : %f\n", (float)b4.Min());
+	printf("小さい値 : %f\n", (double)b5.Min());
+	printf("小さい値 : %f\n", (double)b6.Min());
+
 }
